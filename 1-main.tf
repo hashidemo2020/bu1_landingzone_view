92460e2259477ab53472b2c4e84f4058b3c5ad1d
Ranjit Viswakumar <WhatsARanjit@users.noreply.github.com>
2020-02-20 14:03:20 -0500

    Version 2 of module

--- 1-main.tf
+++ 1-main.tf
@@ -1,6 +1,6 @@
 module "landingzone-view" {
   source  = "app.terraform.io/FMR_BU1/landingzone-view/ecc"
-  version = "1.3.0"
+  version = "2.0.0"
 
   remote_organization = var.remote_organization
   subnet_remote_workspace_name = var.subnet_remote_workspace_name
